e3f512b989a8e9ffe82ab0ff55692d6fa09ac2c4
Thomas L <thomaslomb13@gmail.com>
2020-02-05 19:11:17 +0100

    Idle Events et ajustements camera

--- Assets/Scripts/Physics/REALPlayerMovementBrackeys.cs
+++ Assets/Scripts/Physics/REALPlayerMovementBrackeys.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class REALPlayerMovementBrackeys : MonoBehaviour
 {
     Animator anim;
+    private bool isMooving;
 
     public CharacterControllerBrackeys controller;
     public float runSpeed = 40f;
@@ -12,6 +13,9 @@ public class REALPlayerMovementBrackeys : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+
+
+    private int waitedSeconds = 0; //Seconds waited for our idle animations trigger
 
     private void Start()
     {
@@ -39,13 +43,59 @@ public class REALPlayerMovementBrackeys : MonoBehaviour
     {
         if(Mathf.Abs(horizontalMove) != 0)
         {
-            anim.SetBool("isRunning", true);
+            if (isMooving == false)// Si la dernière valeur de isMooving est fausse
+            {
+                isMooving = true;
+                anim.SetBool("isRunning", true);
+                StopAllCoroutines();
+                waitedSeconds = 0;
+            }
         }
         else
         {
-            anim.SetBool("isRunning", false);
+            if(isMooving == true)// Si la dernière valeur de isMooving est vraie
+            {
+                isMooving = false;
+                anim.SetBool("idleEvent", false);
+                anim.SetBool("isRunning", false);
+                StartCoroutine(idleWaiter());
+            }
         }
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+
+    // Partie jolis Idle
+
+    private IEnumerator idleWaiter()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            anim.SetBool("idleEvent", false);
+            anim.SetBool("idleEvent2", false);
+            anim.SetBool("idleEvent3", false);
+            yield return new WaitForSeconds(1);
+            waitedSeconds = waitedSeconds + 2;
+            if(waitedSeconds >= 16)
+            {
+                switch (Mathf.FloorToInt(Random.Range(0, 3)))
+                {
+                    case 0:
+                        anim.SetBool("idleEvent", true);
+                        break;
+                    case 1:
+                        anim.SetBool("idleEvent2", true);
+                        break;
+                    case 2:
+                        anim.SetBool("idleEvent3", true);
+                        break;
+                    default:
+                        break;
+                }
+                waitedSeconds = 0;
+            }
+        }
     }
 }
