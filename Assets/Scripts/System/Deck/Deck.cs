7bfc0ad7405e3794f6a1e6b665c0e622fba205c9
Yonuko <sacha.eghiazarian@ynov.com>
2020-12-09 18:26:28 +0100

    Ajout des scripts gérant le deck, plus petite démo simple

---
+++ Assets/Scripts/System/Deck/Deck.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+
+    List<Card> deck = new List<Card>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCard(Card card)
+    {
+        deck.Add(card);
+    }
+
+    public void SetDeck(List<Card> cards)
+    {
+        deck = cards;
+    }
+
+    public List<Card> getDeck()
+    {
+        return deck;
+    }
+
+    public void Shuffle()
+    {
+        int n = deck.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n);
+            Card value = deck[k];
+            deck[k] = deck[n];
+            deck[n] = value;
+        }
+    }
+
+    public Card UseCard()
+    {
+        Card card = deck[0];
+        deck.Remove(card);
+        return card;
+    }
+
+}
---
+++ Assets/Scripts/System/Deck/DeckUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckUI : MonoBehaviour
+{
+
+    public Deck deck;
+    public List<Card> cards = new List<Card>();
+
+    public GameObject holder;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck.SetDeck(cards);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Card currentCard = deck.UseCard();
+            currentCard.use();
+            UpdateUI();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            deck.Shuffle();
+            UpdateUI();
+        }
+    }
+
+    void UpdateUI()
+    {
+        while(holder.transform.childCount > 0)
+        {
+            Destroy(holder.transform.GetChild(0));
+        }
+        List<Card> cardsList = deck.getDeck();
+        for (int i = 0; i < cardsList.Count; i++)
+        {
+            GameObject go = Instantiate(prefab, holder.transform);
+            go.transform.GetChild(0).GetComponent<Text>().text = cardsList[i].name;
+            go.transform.GetChild(1).GetComponent<Image>().sprite = cardsList[i].image;
+            go.transform.GetChild(2).GetComponent<Text>().text = "Description : " + cardsList[i].description;
+            go.transform.GetChild(3).GetComponent<Text>().text = "None";
+        }
+    }
+}
