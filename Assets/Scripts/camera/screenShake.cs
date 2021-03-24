804bb97a6646af86311ed36c3f0ec502dd4d8b33
Thomas <thomaslomb13@gmail.com>
2021-03-24 12:25:08 +0100

    Screen shake baby

---
+++ Assets/Scripts/camera/screenShake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class screenShake : MonoBehaviour
+{
+    public static screenShake Instance { get; private set; }
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakerTime;
+
+    private void Awake()
+    {
+        Instance = this;
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shakerTime = time;
+    }
+
+    private void Update()
+    {
+        if(shakerTime > 0)
+        {
+            shakerTime -= Time.deltaTime;
+            if(shakerTime <= 0)
+            {
+                //Time over!
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                    cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+}
